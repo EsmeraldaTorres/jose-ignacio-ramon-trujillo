95838c1e2a2158c0c8dd359335bb2c939d57c9be
Esmeralda Torres Aguirre <estorresag2019@gmail.com>
2025-02-08 14:09:07 -0600

    actulaizar fecha

--- javascript.js
+++ javascript.js
@@ -36,7 +36,7 @@ playPause.addEventListener("click", () => {
 
 // a regresiva -------------------------------------------------------------------
 // Set the date we're counting down to
-var countDownDate = new Date("Mar 16, 2024 19:30").getTime();
+var countDownDate = new Date("Dec 16, 2025 19:30").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
